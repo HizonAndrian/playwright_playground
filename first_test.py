ca0f341988b65d64ceab916a3d335867c90e281b
HizonAndrian <markandrianhizon@gmail.com>
2026-08-03 07:02:51 +0800

    File clearning

--- first_test.py
+++ first_test.py
@@ -1,12 +1,12 @@
-from playwright.sync_api import sync_playwright
+# from playwright.sync_api import sync_playwright
 
-def run():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
-        page.goto("https://google.com")
-        print("Title Page is: " + page.title())
-        browser.close()
+# def run():
+#     with sync_playwright() as p:
+#         browser = p.chromium.launch(headless=True)
+#         page = browser.new_page()
+#         page.goto("https://google.com")
+#         print("Title Page is: " + page.title())
+#         browser.close()
 
-if __name__ == "__main__":
-    run()
+# if __name__ == "__main__":
+#     run()
