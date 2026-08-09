3004b59de5939a00af7fa29a448f6c9ec9d34537
HizonAndrian <markandrianhizon@gmail.com>
2026-08-09 21:35:08 +0800

    Added object type on the parameter: Page

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,8 +1,8 @@
 import re # For Regular expression (Regex)
 import pytest
-from playwright.sync_api import expect
+from playwright.sync_api import Page, expect
 
-def test_google_search(page):
+def test_google_search(page: Page):
     page.goto("https://google.com")
 
     expect(page).to_have_title(re.compile("Google", re.IGNORECASE))
