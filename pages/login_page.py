2d601b45c7e61469a4adb0dbcd77f48146a3f232
HizonAndrian <markandrianhizon@gmail.com>
2026-08-16 19:37:05 +0800

    AT HOME

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,16 +1,14 @@
-class LogInPage:
+class LoginPage:
     def __init__(self, page):
         self.page = page
         self.username_input = page.get_by_placeholder("Username")
         self.password_input = page.get_by_placeholder("Password")
-        self.login_button   = page.get_by_role("button", name="Login")
+        self.login_btn      = page.get_by_role("button", name="Login")
 
-    def goto(self):
-        self.page.goto("https://www.saucedemo.com/")
+    def goto_page(self):
+        self.page.goto()
 
     def login(self, username, password):
         self.username_input.fill(username)
         self.password_input.fill(password)
-        self.login_button.click()
-
-    
+        self.login_btn.click()
